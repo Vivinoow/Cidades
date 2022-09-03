682c28ad35b1d30a187321eb9979b996d20700b0
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-02 21:56:06 -0300

    Cidades

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Santos");
+Console.WriteLine("Sao Vincente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongagua");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Peruibe");
